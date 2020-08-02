4c1f4db9e4f8f7be9b3e11d343f7f28802255ecc
Brandon Barnett <brbarnett@PHNX-MBP.local>
2020-08-02 14:32:31 -0400

    Moving filter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+Vue.filter('currency', (value: number) => `$${value.toFixed(2)}`);
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
