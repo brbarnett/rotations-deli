93093bc6aeed675876df50f65de0fdaacbf5bdd9
Brandon Barnett <brbarnett@PHNX-MBP.local>
2020-08-02 17:00:16 -0400

    Responsive styling for header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,51 @@
 <template>
-  <div class="container-fluid">
-    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/rotations-logo.png" /> Rotations Deli
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
-          <!-- empty -->
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="btn btn-secondary mx-2 d-none d-sm-block" href="https://www.google.com/maps/dir//%2746.5434423,-87.3951857%27" target="_blank">
-              <i class="fa fa-location-arrow" aria-hidden="true"></i> Get Directions
-            </a>
-            <a class="nav-link d-sm-none" href="https://www.google.com/maps/dir//%2746.5434423,-87.3951857%27" target="_blank">
-              <i class="fa fa-location-arrow" aria-hidden="true"></i> Get Directions
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="btn btn-primary d-none d-sm-block" href="tel:906-273-0639">
-              <i class="fa fa-mobile" aria-hidden="true"></i> Call Now
-            </a>
-            <a class="nav-link d-sm-none" href="tel:906-273-0639">
-              <i class="fa fa-mobile" aria-hidden="true"></i> Call Now
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <div class="header">
+    <div class="container fixed-top">
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/rotations-logo.png" /> Rotations Deli
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+            <!-- empty -->
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="btn btn-secondary mx-2 d-none d-sm-block"
+                href="https://www.google.com/maps/dir//%2746.5434423,-87.3951857%27"
+                target="_blank"
+              >
+                <i class="fa fa-location-arrow" aria-hidden="true"></i> Get Directions
+              </a>
+              <a
+                class="nav-link d-sm-none"
+                href="https://www.google.com/maps/dir//%2746.5434423,-87.3951857%27"
+                target="_blank"
+              >
+                <i class="fa fa-location-arrow" aria-hidden="true"></i> Get Directions
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary d-none d-sm-block" href="tel:906-273-0639">
+                <i class="fa fa-mobile" aria-hidden="true"></i> Call Now
+              </a>
+              <a class="nav-link d-sm-none" href="tel:906-273-0639">
+                <i class="fa fa-mobile" aria-hidden="true"></i> Call Now
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -42,8 +57,9 @@ export default class Header extends Vue {}
 </script>
 
 <style scoped lang="scss">
-.container {
+.header {
   height: 56px;
+  margin-bottom: 56px;
 }
 .navbar-brand {
   height: 56px;
