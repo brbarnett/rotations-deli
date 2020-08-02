26232cde064820b5a483f767af816fdacacc8ef6
Brandon Barnett <brbarnett@PHNX-MBP.local>
2020-08-02 15:09:26 -0400

    Adding content to banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container">banner</div>
+  <div class="container">
+    <h1>Rotations Deli</h1>
+    <p>Unique deli with rotating specials in Marquette</p>
+    <img class="img-fluid" src="https://via.placeholder.com/1080x608" alt="Rotations Deli" />
+    <a class="btn btn-primary" href="https://www.google.com/maps/dir//%2746.5434423,-87.3951857%27">Get directions</a>
+  </div>
 </template>
 
 <script lang="ts">
