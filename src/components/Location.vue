bd690b9c1a583a80eab3587acc725f503558a137
Brandon Barnett <brbarnett@PHNX-MBP.local>
2020-08-02 17:00:31 -0400

    Adding google map

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,6 +1,14 @@
 <template>
   <section class="container">
     <h2>Location</h2>
+
+    <iframe
+      id="map"
+      frameborder="0"
+      style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAcY9eSFOl85dNDg6BrYv82vV5tzOx0lms&q=113+S.+3rd+Street,+Marquette,+MI+49855"
+      allowfullscreen
+    ></iframe>
   </section>
 </template>
 
@@ -12,3 +20,11 @@ import './section.scss';
 @Component
 export default class Location extends Vue {}
 </script>
+
+<style scoped lang="scss">
+#map {
+  height: 400px;
+  margin-bottom: 40px;
+  width: 100%;
+}
+</style>
