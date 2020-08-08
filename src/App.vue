4885464779d81671a093d0e5104fe7b810cdcd8f
Brandon Barnett <brbarnett@PHNX-MBP.local>
2020-08-08 08:36:42 -0400

    Meta tags

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
     <Menu />
     <Location />
     <Footer />
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   </div>
 </template>
 
@@ -17,6 +16,7 @@ import Header from './components/Header.vue';
 import Location from './components/Location.vue';
 import Menu from './components/Menu.vue';
 
+Component.registerHooks(['metaInfo']);
 @Component({
   components: {
     Banner,
@@ -26,7 +26,35 @@ import Menu from './components/Menu.vue';
     Menu,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  /* eslint-disable lines-between-class-members */
+  private description = 'Unique deli with rotating specials in Marquette, MI';
+  /* eslint-disable global-require */
+  private logo = `${window.location.protocol}//${window.location.host}${require('./assets/rotations-entry.jpg')}`;
+  /* eslint-enable global-require */
+  private title = 'Rotations Deli';
+  /* eslint-enable lines-between-class-members */
+
+  public metaInfo() {
+    return {
+      title: 'Rotations Deli',
+      meta: [
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:description', content: this.description },
+        // image must be an absolute path
+        { name: 'twitter:image', content: this.logo },
+        // Facebook OpenGraph
+        { property: 'og:title', content: this.title },
+        { property: 'og:site_name', content: this.title },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: this.logo },
+        { property: 'og:description', content: this.description },
+      ],
+    };
+  }
+}
 </script>
 
 <style lang="scss">
@@ -34,8 +62,6 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   color: #2c3e50;
-  // margin-top: 60px;
 }
 </style>
