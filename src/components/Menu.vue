a578af010d0842d6e4dc8adabf50e5e2c23d69b2
Brandon R. Barnett <6238678+brbarnett@users.noreply.github.com>
2021-05-06 14:01:39 -0500

    Menu update

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,9 +3,17 @@
     <h2>Menu</h2>
 
     <div class="row">
-      <div v-for="item in menuItems" :key="item.name" class="col-md-6 col-lg-4 menu-item">
+      <div
+        v-for="item in menuItems"
+        :key="item.name"
+        class="col-md-6 col-lg-4 menu-item"
+      >
         <div class="card">
-          <img v-bind:src="item.image" class="card-img-top" v-bind:alt="item.name" />
+          <img
+            v-bind:src="item.image"
+            class="card-img-top"
+            v-bind:alt="item.name"
+          />
           <div class="card-body">
             <h5 class="card-title">{{ item.name }}</h5>
             <p class="card-text">{{ item.description }}</p>
@@ -35,66 +43,66 @@ export default class Menu extends Vue {
   private menuItems: MenuItem[] = [
     {
       description:
-        'Gyro: Spiced lamb cooked on a vertical rotisserie, topped with tzatziki sauce, tomatoes and onion on a pita.',
+        'Cheese Steak: Ribeye, Caramelized onions, with your choice of Cheez-Whiz® or Provolone cheese, on a Hoagie roll.',
+      image: require('../assets/menu/dave.jpg'),
+      name: 'The Dave',
+      price: 10.75,
+    },
+    {
+      description:
+        'Gyro: Spiced Lamb, Onion, Tomato, Tzatziki sauce, on Pita Bread.',
       image: require('../assets/menu/steve.jpg'),
       name: 'The Steve',
+      price: 7.5,
+    },
+    {
+      description:
+        'Grilled Ham/Cheese on Pita Bread: your choice of Swiss, Provolone, or Mozzarella.',
+      image: require('../assets/menu/jason.jpg'),
+      name: 'The Jason',
+      price: 7.75,
+    },
+    {
+      description:
+        'Pulled Pork Tacos (3): Smoky Pork, Chihuahua cheese, cilantro/lime relish, and Arugula on corn tortillas.',
+      image: require('../assets/menu/emily.jpg'),
+      name: 'The Emily',
+      price: 8.5,
+    },
+    {
+      description:
+        'Chicken Shawarma: Middle-Eastern spiced chicken, Cucumber, Fresh Parsley, Arugula, on Pita Bread, with your choice of Tzatziki or Tahini Sauce.',
+      image: require('../assets/menu/chris.jpg'),
+      name: 'The Chris',
+      price: 8.25,
+    },
+    {
+      description:
+        'Falafel: Deep fried Chickpea Fritters, Tomato, Green Onion, Arugula, on Pita Bread, with your choice of Tzatziki or Tahini sauce.',
+      image: require('../assets/menu/ann.jpg'),
+      name: 'The Ann',
       price: 6.5,
     },
     {
       description:
-        'Philly cheese steak: Paper thin ribeye steak, onions, and choice of Provolone or Cheez-Whiz on a hoagie roll.',
-      image: require('../assets/menu/dave.jpg'),
-      name: 'The Dave',
+        'Pastrami: Pastrami on Dark Rye with Spicy Brown Mustard and Swiss Cheese.',
+      image: require('../assets/menu/joey.jpg'),
+      name: 'The Joey',
       price: 9.75,
     },
     {
       description:
-        'Grilled ham and cheese: Our version of a classic, thinly sliced ham, with your choice of cheese, grilled between two pieces of pita bread.',
-      image: require('../assets/menu/jason.jpg'),
-      name: 'The Jason',
-      price: 6.75,
-    },
-    {
-      description:
-        'Cilantro-pork taco: Smokey pulled pork with a tangy cilantro-lime relish, topped with Chihuahua cheese, green onions, cilantro, and arugula in corn tortilla.',
-      image: require('../assets/menu/emily.jpg'),
-      name: 'The Emily',
-      price: 7.25,
-    },
-    {
-      description:
-        'Pastrami: Classic deli sandwich, thinly sliced pastrami, swiss cheese, spicy brown mustard on grilled rye bread.',
-      image: require('../assets/menu/joey.jpg'),
-      name: 'The Joey',
-      price: 9.0,
-    },
-    {
-      description:
-        'Falafel: Deep fried chickpea fritters nestled in a pita and topped with roma tomatoes, arugula, green onion with a choice of sauce: tzatziki or tahini.',
-      image: require('../assets/menu/ann.jpg'),
-      name: 'The Ann',
-      price: 5.5,
-    },
-    {
-      description:
-        'Grilled turkey breast with goat cheese, arugula and a spicy cranberry sauce inside focaccia bread.',
+        'Grilled Turkey, Goat Cheese, Spicy Cranberry sauce, Arugula, on Focaccia Bread.',
       image: require('../assets/menu/becky.jpg'),
       name: 'The Becky',
-      price: 9.0,
+      price: 9.75,
     },
     {
       description:
-        'Reuben: Corned beef, sauerkraut, swiss cheese and and Thousand Island dressing on dark rye.',
+        'Reuben: Corned beef, Sauerkraut, Swiss Cheese, 1,000 Island Dressing on Dark Rye.',
       image: require('../assets/menu/reuben.jpg'),
       name: 'The Ruby',
-      price: 9.0,
-    },
-    {
-      description:
-        'Chicken shawarma with cucumber, arugula, parsley and a choice of tzatziki or tahini.',
-      image: require('../assets/menu/chris.jpg'),
-      name: 'The Chris',
-      price: 7.75,
+      price: 9.75,
     },
   ];
   /* eslint-enable global-require */
